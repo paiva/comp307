392602646959ffc0ded1310e21a9255ac85f430f
Santiago Paiva <paiva.santiago@gmail.com>
2014-10-19 14:42:37 -0400

    fixed bug in jquery

--- js/jquery.js
+++ js/jquery.js
@@ -9,20 +9,23 @@ $(document).ready(function(){
         type: "POST",
         url: "data.json",
         dataType: "json",
+
         success: function(data){
           $.each(data,function(key,value){
             if(username == value.username && password == value.password){
               error = false;
-              document.location="save_ss.php?user_login=" + user;
-            }else{
+            }
+          });
+          if(error = false){
+            document.location="save_ss.php?user_login=" + username;
+          }
+            else{
                 $('#containter').slideUp('slow').slideDown("slow");
                 $('#username').val('');
                 $('#password').val('');
                 }
-              }
-          });
-        return false;
+        }
     });
-
+    return false;
   });
 });
