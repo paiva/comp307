d525f194953b9917812be8596bca6f5dea4fbfc6
Santiago Paiva <paiva.santiago@gmail.com>
2014-10-25 15:57:23 -0400

    Modifications on page.html

--- page.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  if(empty($_SESSION['user'])){
-    header('Location: index.php');
-  }else{
-    echo "welcome" .$_SESSION['user'] ."<br/><a href='logout.php'>Logout</a>";
-  }
-?>
