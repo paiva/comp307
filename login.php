787920d4789cfc57925fc1f5796d6b812c856e49
Santiago Paiva <paiva.santiago@gmail.com>
2014-10-24 23:49:36 -0400

    fixed some bugs in login script

--- login.php
+++ login.php
@@ -1,35 +1,37 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'practice');
+define('DB_NAME', 'Q2DB');
 define('DB_USER','root');
 define('DB_PASSWORD','');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
 /*
 $ID = $_POST['user'];
-$Password = $_POST['pass'];
+$Password = $_POST['password'];
 */
+
 function SignIn()
 {
-session_start();   //starting the session for user profile page
-if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
-{
-	$query = mysql_query("SELECT *  FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-	$row = mysql_fetch_array($query) or die(mysql_error());
-	if(!empty($row['userName']) AND !empty($row['pass']))
-	{
-		$_SESSION['userName'] = $row['pass'];
-		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
-    // header('Location: page.php');
+  session_start();   //starting the session for user profile page
+  if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+  {
+  	$query = mysql_query("SELECT *  FROM members where username = '$_POST[user]' AND password = '$_POST[password]'") or die(mysql_error());
+  	$row = mysql_fetch_array($query) or die(mysql_error());
+  	if(!empty($row['username']) AND !empty($row['password']))
+  	{
+  		$_SESSION['username'] = $row['password'];
+  		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+      // header('Location: page.php');
 
-	}
-	else
-	{
-		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
-	}
-}
+  	}
+  	else
+  	{
+  		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+  	}
+  }
 }
 if(isset($_POST['submit']))
 {
