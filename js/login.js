c09bbd71eabeffda7deca10fc596eeaac012d6b6
Santiago Paiva <paiva.santiago@gmail.com>
2014-10-28 10:13:44 -0400

    added encrypt function

--- js/login.js
+++ js/login.js
@@ -2,18 +2,46 @@ $(document).ready(function(){
 
     $('#errDiv').hide();
     $('#login').click(function(e){
-      e.preventDefault();
+    e.preventDefault();
 
-      var username=$('#username').val();
-      var password=$('#password').val();
-      var str_json = JSON.stringify({
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    var str_json = JSON.stringify({
                                   "username": username,
                                   "password" : password
                                 });
 
-      var request = new XMLHttpRequest();
-      request.open("POST", "login.php", true)
-      request.setRequestHeader("Content-type", "application/json")
-      request.send(str_json)
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost/307/A2/login.php", true);
+    //request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.setRequestHeader("Content-type", "application/json");
+    request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+      key = "";
+      key = request.responseText;
+        if(key != "" ){
+            console.log('here I am');
+            password = encrypt(password,key);
+            //jsonAndSend();
+        } else{
+            document.getElementById("errDiv").innerHTML = "Username does not exist";
+            }
+      }
+      request.send("username="+username);
+      };
+
+    function encrypt(input, key) {
+      key =  (26 - key) % 26;
+      var output = "";
+      for (var i = 0; i < input.length; i++) {
+          var c = input.charCodeAt(i);
+            if      (c >= 65 && c <=  90) output += String.fromCharCode((c - 65 + key) % 26 + 65);  // Uppercase
+              else if (c >= 97 && c <= 122) output += String.fromCharCode((c - 97 + key) % 26 + 97);  // Lowercase
+                else                          output += input.charAt(i);  // Copy
+      }
+      return output;
+    };
+
     });
 });
